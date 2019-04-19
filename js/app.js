2f923a353842ebdf11af3b650a44a9521f3e65d9
Jon <jonharwell@JMBA.local>
2019-04-18 17:06:53 -1000

    1-6

--- js/app.js
+++ js/app.js
@@ -2,17 +2,36 @@
 
 //1. In the nav element, target the span element with the id of 'matCount' and change the number from 0 to 11.
 
+matCount.innerHTML = 11;
+
 //2. In the nav element, target the span element with the id of 'msgCount' and change the number from 0 to 23.
+
+msgCount.innerHTML = 23;
 
 //3. In the div id data section, target the div element with the id of 'fullname' and change Full Name to Ronald McDonald.
 
+fullname.innerHTML = 'Ronald McDonald';
+
 //4. In the div id data section, target the div element with the id of 'age' and change Age to 63 Years Old.
+
+age.innerHTML = '63 Years Old';
 
 //5. In the div id data section, create a div with an id of job and give it the following information: Clown and Restauranteur.
 
+var jobBox = document.createElement('div');
+jobBox.id = 'job';
+jobBox.innerHTML = 'Clown and Restauranteur';
+data.appendChild(jobBox);
+
 //6. In the div id data section, create a div with an id of hobbies and give it the following information: Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.
 
+var hobbiesBox = document.createElement('div');
+hobbiesBox.id = 'hobbies';
+hobbiesBox.innerHTML = 'Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King';
+data.appendChild(hobbiesBox);
+
 //7. In the div id data section, create a div with an id of location and give it the following information: Honolulu, HI.
+
 
 //8. In the div id data section, create a div with an id of wants and give it the following information: Looking for a Mrs. McDonald.
 
